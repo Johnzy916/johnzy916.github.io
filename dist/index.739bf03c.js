0b5ff39b0bde9ddb21e7d5688bd78b8f07b79635
Jhonathan Angus <jhonathan.angus@gmail.com>
2022-02-17 21:57:00 -0800

    add particles

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -522,18 +522,6 @@ function hmrAcceptRun(bundle, id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _heroJs = require("./components/hero.js");
 var _heroJsDefault = parcelHelpers.interopDefault(_heroJs);
-// Mouse tracking
-//////////////////////////
-const mouse = {
-    x: null,
-    y: null,
-    radius: 150
-};
-// get mouse coords
-document.addEventListener('mousemove', (e)=>{
-    mouse.x = e.x;
-    mouse.y = e.y;
-});
 // Hero
 /////////////////////////
 _heroJsDefault.default();
@@ -543,18 +531,46 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 exports.default = ()=>{
     const canvas = document.querySelector('#canvas-hero');
+    const canvasContainer = document.querySelector('#canvas-container');
     const ctx = canvas.getContext('2d');
-    // set size to window
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    // set size to container
+    const resizeCanvas = ()=>{
+        canvas.width = canvasContainer.clientWidth;
+        canvas.height = canvasContainer.clientHeight;
+    };
+    resizeCanvas();
+    window.addEventListener('resize', resizeCanvas);
+    // track mouse movement
+    const mouse = {
+        x: null,
+        y: null,
+        radius: 100
+    };
+    // get mouse coords
+    canvas.addEventListener('mousemove', (e)=>{
+        mouse.x = e.offsetX;
+        mouse.y = e.offsetY;
+    });
     // create particle array
-    let pArray = [];
+    let pixelArray = [];
+    // constants
+    const TEXT_COLOR = 'white';
+    const PARTICLE_COLOR = 'white';
+    const PARTICLE_SPREAD = 10;
+    const PARTICLE_ADJUST_X = 9;
+    const PARTICLE_ADJUST_Y = 20;
+    const LINE_CONNECT_DISTANCE = 10;
+    const LINE_CONNECT_WIDTH = 2;
+    const LINE_CONNECT_COLOR = 'white';
     // draw text
-    ctx.fillStyle = 'white';
-    ctx.font = '30px Verdana';
-    ctx.fillText('Web', 0, 40);
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.font = '20px Verdana';
+    ctx.fillText('Jhonathan', 0, 20);
+    // ctx.rect(0, 0, 105, 25);
+    // ctx.strokeStyle = 'white';
+    // ctx.stroke();
     // get image data
-    const pixelData = ctx.getImageData(0, 0, 100, 100);
+    const pixelData = ctx.getImageData(0, 0, 105, 25);
     // particle class
     class Particle {
         constructor(x, y){
@@ -566,7 +582,7 @@ exports.default = ()=>{
             this.density = Math.random() * 30 + 1;
         }
         draw() {
-            ctx.fillStyle = 'white';
+            ctx.fillStyle = PARTICLE_COLOR;
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
             ctx.closePath();
@@ -601,21 +617,39 @@ exports.default = ()=>{
     const initParticles = ()=>{
         pixelArray = [];
         for(let y = 0; y < pixelData.height; y++){
-            for(let x = 0; x < pixelData.width; x++)if (pixelData.data[y * 4 * data.width + x * 4 + 3] > 128) {
-                const positionX = x;
-                const positionY = y;
-                pixelArray.push(new Particle(x, y));
+            for(let x = 0; x < pixelData.width; x++)if (pixelData.data[y * 4 * pixelData.width + x * 4 + 3] > 128) {
+                const positionX = x + PARTICLE_ADJUST_X;
+                const positionY = y + PARTICLE_ADJUST_Y;
+                pixelArray.push(new Particle(positionX * PARTICLE_SPREAD, positionY * PARTICLE_SPREAD));
             }
         }
     };
     initParticles();
+    // connect particles
+    const connectParticles = ()=>{
+        let opacity = 1;
+        for(let a = 0; a < pixelArray.length; a++)for(let b = a; b < pixelArray.length; b++){
+            let dx = pixelArray[a].x - pixelArray[b].x;
+            let dy = pixelArray[a].y - pixelArray[b].y;
+            let distance = Math.hypot(dx, dy);
+            if (distance < LINE_CONNECT_DISTANCE) {
+                ctx.strokeStyle = LINE_CONNECT_COLOR;
+                ctx.lineWidth = LINE_CONNECT_WIDTH;
+                ctx.beginPath();
+                ctx.moveTo(pixelArray[a].x, pixelArray[a].y);
+                ctx.lineTo(pixelArray[b].x, pixelArray[b].y);
+                ctx.stroke();
+            }
+        }
+    };
     // animate particles
     const animateParticles = ()=>{
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        for (particle of pArray){
+        for (particle of pixelArray){
             particle.draw();
             particle.update();
         }
+        // connectParticles();
         requestAnimationFrame(animateParticles);
     };
     animateParticles();
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,5 @@
-import initHeroComponent from './components/hero.js';
-
-// Mouse tracking
-//////////////////////////
-const mouse = {
-    x: null,
-    y: null,
-    radius: 150
-}
-// get mouse coords
-document.addEventListener('mousemove', (e) => {
-    mouse.x = e.x;
-    mouse.y = e.y;
-});
+import heroComponentInit from './components/hero.js';
 
 // Hero
 /////////////////////////
-initHeroComponent();
+heroComponentInit();
