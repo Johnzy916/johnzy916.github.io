3983f32dd9ea77f0332d43c61dee9e3b45a02b68
Jhonathan Angus <jhonathan.angus@gmail.com>
2022-03-14 15:04:55 -0700

    reduce about me size

--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -1,9 +1,30 @@
 import mixitup from 'mixitup';
 
 export default async () => {
+    // add event listeners
+    const filterControls = Array.from(document.querySelectorAll('.project-controls > *'));
+    filterControls.forEach(control => {
+        control.addEventListener('change', handleControlChange);
+    })
     const mixer = mixitup('.project-grid', {
         selectors: {
             target: '.project-card'
+        },
+        animation: {
+            animateResizeTargets: true
         }
     });
+    function handleControlChange() {
+        if (this.checked) {
+            // uncheck other controls
+            filterControls.forEach(control => {
+                if (control !== this) {
+                    control.checked = false;
+                }
+            });
+            const lang = this.dataset.lang;
+            if (lang == 'all') return mixer.show();
+            else return mixer.filter(`.lang-${lang}`);
+        }
+    }
 };
