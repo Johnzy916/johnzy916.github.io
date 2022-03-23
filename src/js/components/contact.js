75936abd1512649f47e7605976fe249db8a113ac
Jhonathan Angus <jhonathan.angus@gmail.com>
2022-03-22 20:05:54 -0700

    add animation to contact

--- src/js/components/contact.js
+++ src/js/components/contact.js
@@ -8,20 +8,33 @@ export default () => {
     const ctx2 = canvas2.getContext('2d');
 
     const canvasWidth = canvas2.width = contactFlexbox.offsetWidth / 2;
-    const canvasHeight = canvas2.height = contactForm.offsetHeight;
+    const canvasHeight = canvas2.height = contactForm.offsetHeight * 1.5;
 
     const alienImage = new Image();
-    alienImage.src = alienImageSrc;
+    alienImage.src =alienImageSrc;
 
-    const spriteWidth = spriteAnimations['idle'].loc[0].width;
-    const spriteHeight = spriteAnimations['idle'].loc[0].height;
-    const spriteX = spriteAnimations['idle'].loc[0].x;
-    const spriteY = spriteAnimations['idle'].loc[0].y;
+    let currentState = 'idle';
+    let currentFrame = 0;
+    let gameFrame = 0;
+    const staggerFrames = 7;
 
     function animate() {
         ctx2.clearRect(0, 0, canvasWidth, canvasHeight);
-        // ctx2.fillText('before', 50, 50)
-        ctx2.drawImage(alienImage, spriteX, spriteY, spriteWidth, spriteHeight, 50, 50, spriteWidth, spriteHeight);
+
+        let spriteLocations = spriteAnimations[currentState].loc;
+        let currentSprite = spriteLocations[currentFrame];
+        let { width, height, x, y } = currentSprite;
+        let centerHorizontal = (canvasWidth / 2) - (width / 2);
+        let floorVertical = canvasHeight - (height + 10);
+
+        ctx2.drawImage(alienImage, x, y, width, height, centerHorizontal, floorVertical, width, height);
+        if (gameFrame % staggerFrames == 0) {
+            if (currentFrame < spriteLocations.length - 1) currentFrame++;
+            else currentFrame = 0;
+        }
+
+        gameFrame++;        
+        requestAnimationFrame(animate)
     }
     animate()
 }
