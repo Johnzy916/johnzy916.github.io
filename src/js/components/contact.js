3b1ff02df58fa30f9ea4bc50e83a130850b09c3d
Jhonathan Angus <jhonathan.angus@gmail.com>
2022-03-23 22:29:00 -0700

    add animation

--- src/js/components/contact.js
+++ src/js/components/contact.js
@@ -13,6 +13,7 @@ export default () => {
 
     const canvasBox = canvas2.getBoundingClientRect();
 
+    // track mouse
     const mouse = {
         x: null,
         y: null
@@ -23,55 +24,89 @@ export default () => {
 		mouse.y = e.clientY;
 	});
 
+    // create images
     const alienImage = new Image();
     alienImage.src = alienImageSrc;
     const alienReversedImage = new Image();
     alienReversedImage.src = alienReversedImageSrc;
 
+    // simple state
     let currentState = 'run';
-    let previousState = null;
     let gameFrame = 0;
-    const staggerFrames = 8;
+    let staggerFrames = 8;
+    let actionFrame = 0;
+    let active = false;
     let isHit = false;
     let hitCount = 0;
     let isDead = false;
 
     canvas2.addEventListener('click', () => {
-        currentState = 'hit';
-        isHit = true;
-        previousState = currentState;
-        hitCount++;
-    })
+        active = true;
+        if (hitCount < 1) {
+            isDead = false;
+            isHit = true;
+            hitCount++;
+        } else {
+            isDead = true;
+            hitCount = 0;
+        }
+    });
 
     function animate() {
         ctx2.clearRect(0, 0, canvasWidth, canvasHeight);
         // sprite location
         let spriteLocations = spriteAnimations[currentState].loc;
+        // slow animation, loop through length
         let currentFrame = Math.floor(gameFrame/staggerFrames) % spriteLocations.length;
+        // get sprite data
         let currentSprite = spriteLocations[currentFrame];
         let { width, height, x, y } = currentSprite;
+        // center sprite in canvas
         let centerHorizontal = (canvasWidth / 2) - (width / 2);
-        let floorVertical = canvasHeight - (height + 10);
+        let centerVertical = (canvasHeight / 2) - (height / 2);
 
-        // get distance
+        // hitbox
+        let box = {
+            top: canvasBox.top + centerVertical,
+            bottom: canvasBox.top + centerVertical + height,
+            left: canvasBox.left + centerHorizontal,
+            right: canvasBox.left + centerHorizontal + width
+        };
+        // ctx2.strokeRect(centerHorizontal, centerVertical, width, height);
+
+        // get distance to mouse
         const dx = mouse.x - (canvasBox.left + (canvasBox.width / 2));
         const dy = mouse.y - (canvasBox.top + (canvasBox.height / 2));
-        const distance = Math.hypot(dx, dy);
+        const distance = Math.hypot(dx, dy) - (width / 2);
 
-        // change direction
-        const isLeftOfImage = mouse.x < canvasBox.left + (canvasBox.width / 2);
-        const currentImage = isLeftOfImage ? alienReversedImage : alienImage;
-        ctx2.drawImage(currentImage, x, y, width, height, centerHorizontal, floorVertical, width, height);
-
-        // respond to click
-        if (currentState == 'hit' && currentFrame == spriteLocations.length) {
-            currentState = previousState;
+        // finish current action
+        if (active) {
+            if (actionFrame < (spriteLocations.length * staggerFrames)) {
+                currentState = isDead ? 'dead' : 'hurt';
+                actionFrame++;
+            } else {
+                actionFrame = 0;
+                active = false;
+            }
+        // else return to normal
+        } else {
+            // inside hitbox
+            if (mouse.x > box.left &&
+                mouse.x < box.right &&
+                mouse.y < box.bottom &&
+                mouse.y > box.top) {
+                    currentState = 'idle';
+            } else {
+                // respond to mouse distance
+                if (distance < 200) currentState = 'walk';
+                else currentState = 'run';
+            }
         }
 
-        // update sprite
-        if (distance < 200) currentState = 'idle';
-        else if (distance < 400) currentState = 'walk';
-        else currentState = 'run';
+        // change sprite direction
+        const isLeftOfImage = mouse.x < canvasBox.left + (canvasBox.width / 2);
+        const currentImage = isLeftOfImage ? alienReversedImage : alienImage;
+        ctx2.drawImage(currentImage, x, y, width, height, centerHorizontal, centerVertical, width, height);
 
         // progress animation
         gameFrame++;        
